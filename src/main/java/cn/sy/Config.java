ba5301915bd01075e01e8899bb748c3e38a23b5f
Sy9876 <shenyue_sam@163.com>
2018-01-27 12:19:58 +0800

    add spring-security depandency

--- src/main/java/cn/sy/Config.java
+++ src/main/java/cn/sy/Config.java
@@ -1,10 +1,12 @@
 package cn.sy;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableTransactionManagement
+@EnableWebSecurity
 public class Config {
 
 	
