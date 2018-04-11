657bd27fdb244f37d26ca1191182c4ee2d14c0a9
shenyue <sheny@lzt.com.cn>
2018-04-11 17:27:38 +0800

    getSupportCityString work

--- src/main/java/cn/sy/ws/client/MyWsClient.java
+++ src/main/java/cn/sy/ws/client/MyWsClient.java
@@ -1,24 +1,77 @@
 package cn.sy.ws.client;
 
+import java.net.URL;
 import java.util.List;
+
+import javax.xml.namespace.QName;
+import javax.xml.ws.Service;
+import javax.xml.ws.soap.SOAPBinding;
 
 import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
 
 public class MyWsClient {
-
-	public static void invoke() {
+	/*
+	public static void invoke2() {
 		try {
+//			String addr = "http://ws.webxml.com.cn/WebServices/WeatherWS.asmx";
+			String addr = "http://localhost:8089/mockWeatherWSSoap";
 			System.out.println("start invoke");
 			JaxWsProxyFactoryBean factory = new JaxWsProxyFactoryBean();
 			factory.setServiceClass(WsSEI.class);
-			factory.setAddress("http://ws.webxml.com.cn/WebServices/WeatherWebService.asmx");
+			factory.setAddress(addr);
 		    WsSEI sv = (WsSEI) factory.create();
 	
 		    System.out.println("got sv");
 //		    List<String> result = sv.getWeatherbyCityName("北京");
-		    List<String> result = sv.getSupportCity(null);
-			
+		    
+//		    GetSupportCityStringSoapIn in = new GetSupportCityStringSoapIn();
+//		    in.setTheRegionCode("311101");
+//		    GetSupportCityStringSoapOut result = sv.getSupportCityString(in);
+		    
+//		    GetSupportCityStringSoapOut result = sv.getSupportCityString("311101");
+		    
+//		    List<String> result = sv.getSupportCityString("311101");
+		    
+		    GetSupportCityStringResult result = sv.getSupportCityString("311101");
+		    
 		    System.out.println(result);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	*/
+	public static void invoke() {
+		try {
+			
+			System.out.println("start invoke");
+			
+			QName serviceName = new QName("http://WebXml.com.cn/", "WeatherWS");
+			QName portName = new QName("http://WebXml.com.cn/", "WeatherWSSoap");
+			String addr = "http://ws.webxml.com.cn/WebServices/WeatherWS.asmx";
+//			String addr = "http://localhost:8088/mockWeatherWSSoap";
+			Service sv = Service.create(new URL(addr + "?wsdl"), serviceName);
+			sv.addPort(portName, SOAPBinding.SOAP12HTTP_BINDING, addr);
+			
+			WsSEI proxy = sv.getPort(WsSEI.class);
+			
+		    System.out.println("got sv");
+//		    List<String> result = sv.getWeatherbyCityName("北京");
+		    
+		    GetSupportCityStringSoapIn in = new GetSupportCityStringSoapIn();
+//		    in.setTheRegionCode("311101");
+//		    GetSupportCityStringSoapOut result = sv.getSupportCityString(in);
+		    
+//		    GetSupportCityStringSoapOut result = proxy.getSupportCityString("311101");
+		    
+//		    List<String> result = proxy.getSupportCityString("311101");
+			
+		    GetSupportCityStringResult result = proxy.getSupportCityString("311101");
+		    if(result!=null) {
+		    	for(String s : result.getString()) {
+		    		System.out.println(s);
+		    	}
+		    }
+		    System.out.println("invoke end");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
--- src/main/java/cn/sy/ws/client/WsSEI.java
+++ src/main/java/cn/sy/ws/client/WsSEI.java
@@ -6,15 +6,25 @@ import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
+import javax.xml.ws.ResponseWrapper;
 
-@WebService(name="WeatherWebServiceSoap")
+@WebService(name="WeatherWSSoap", targetNamespace="http://WebXml.com.cn/")
 public interface WsSEI {
 
 	@WebMethod(action="http://WebXml.com.cn/getWeatherbyCityName",
 			operationName="getWeatherbyCityName")
 	public @WebResult(name="getWeatherbyCityNameResult") List<String> getWeatherbyCityName(@WebParam(name="theCityName") String theCityName);
 
-	@WebMethod(action="http://WebXml.com.cn/getSupportCity",
-			operationName="getSupportCity")
-	public @WebResult(name="getSupportCityResult") List<String> getSupportCity(@WebParam(name="byProvinceName") String byProvinceName);
+	@WebMethod(action="http://WebXml.com.cn/getSupportCityString",
+			operationName="getSupportCityString")
+//	public @WebResult(name="GetSupportCityStringSoapOut") GetSupportCityStringSoapOut getSupportCityString(@WebParam(name="getSupportCityString") GetSupportCityStringSoapIn in);
+//	public 
+//		GetSupportCityStringSoapOut getSupportCityString(
+//				@WebParam(name="theRegionCode", targetNamespace="http://WebXml.com.cn/") String theRegionCode);
+
+//	@ResponseWrapper(localName="GetSupportCityStringResponse", targetNamespace="http://WebXml.com.cn/",
+//		className="cn.sy.ws.client.GetSupportCityStringResult")
+	public 
+	GetSupportCityStringResult getSupportCityString(
+			@WebParam(name="theRegionCode") String theRegionCode);
 }
