e7276b5eeddf497eed688469430e2bfdab5df351
shenyue <sheny@lzt.com.cn>
2018-06-14 14:15:38 +0800

    v1

--- src/main/java/cn/sy/controller/KafkaMsgController.java
+++ src/main/java/cn/sy/controller/KafkaMsgController.java
@@ -1,28 +1,14 @@
 package cn.sy.controller;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 
 import javax.annotation.PostConstruct;
 
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.header.Header;
-import org.apache.kafka.common.header.internals.RecordHeaders;
-import org.apache.kafka.common.utils.Utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.support.GenericMessage;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import cn.sy.dto.Demo2KafkaDto;
@@ -36,7 +22,12 @@ public class KafkaMsgController {
 	@Autowired
 	private KafkaUtil kafkaUtil;
 	
-    
+	// 使用@PostConstruct在stringRedisTemplate被注入之后执行redisRoutingMap的初始化
+	@PostConstruct
+	public void init() {
+		logger.info("PostConstruct. init ");
+	}
+
     @RequestMapping("/public/sendKafkaMsgStr.do")
     public void sendKafkaMsgStr() {
 
@@ -79,44 +70,9 @@ public class KafkaMsgController {
     	dto.setTs(new Date());
     	dto.setPayloadStr("sendKafkaMsg2.do");
     	
-//    	MyKafkaMessage<Demo2KafkaDto> msg = new MyKafkaMessage<Demo2KafkaDto>();
-//    	msg.setPayload(dto);
-    	    	
     	logger.info("sendKafkaMsg2.do use kafkaUtil send dto: " + dto);
     	kafkaUtil.send("myTopic", dto);
 
     }
-
-    
-    @RequestMapping("/public/sendDto2List.do")
-    public void sendDto2List() {
-
-    	List<Demo2KafkaDto> list = new ArrayList<>();
-    	
-    	Demo2KafkaDto dto = new Demo2KafkaDto();
-    	dto.setId(UUID.randomUUID().toString());
-    	dto.setFrom("from");
-    	dto.setTo("to");
-    	dto.setTs(new Date());
-    	dto.setPayloadStr("sendDto2List.do");
-    	list.add(dto);
-    	
-    	dto = new Demo2KafkaDto();
-    	dto.setId(UUID.randomUUID().toString());
-    	dto.setFrom("from");
-    	dto.setTo("to");
-    	dto.setTs(new Date());
-    	dto.setPayloadStr("222");
-    	list.add(dto);
-    	
-    	
-//    	MyKafkaMessage<Demo2KafkaDto> msg = new MyKafkaMessage<Demo2KafkaDto>();
-//    	msg.setPayload(dto);
-    	    	
-    	logger.info("sendDto2List.do use kafkaUtil send dto: " + list);
-    	kafkaUtil.send("myTopic", list);
-
-    }
-
 
 }
--- src/main/java/cn/sy/kafka/MyKafkaListener.java
+++ src/main/java/cn/sy/kafka/MyKafkaListener.java
@@ -26,52 +26,12 @@ public class MyKafkaListener {
 	private static Logger logger = LoggerFactory.getLogger(KafkaListener.class);
 
 	
-//	@Autowired
-//	private KafkaTemplate<String, String> kafkaTemplate;
-	
-	
-	/**
-	 * 使用@KafkaListener(topics = "myTopic") 声明多个方法，只有一个能够接收信息（因为group相同），
-	 * 但，如果方法参数类型不同，则根据消息的类型调用相应的方法。
-	 * 例如
-	 *  @KafkaListener(id = "multi", topics = "myTopic")
-	 *  static class MultiListenerBean {
-	 *  	@KafkaHandler
-	 *  	public void listen(String foo) {
-	 *  		...
-	 *  	}
-	 *  	@KafkaHandler
-	 *  	public void listen(Integer bar) {
-	 *  		...
-	 *  	}
-	 *  }
-	 * 
-	 * 使用@KafkaListener(topics = "myTopic", groupId="xx") 声明多个方法，
-	 * 只要groupId不同，则都能够接收信息
-	 * 
-	 * 
-	 */
-	
-//	@KafkaListener(topics = "myTopic", groupId="g1"
-//			 ,containerFactory = "kafkaJsonListenerContainerFactory"
-//			)
-
-//	@KafkaListener(topics = "myTopic", groupId="g1")
 	@KafkaHandler()
 	public void listen3(String payload) throws Exception { 
 	
 		logger.info("listen3 myTopic payload: " + payload);
 
 	}
-    
-//	@KafkaListener(topics = "myTopic", groupId="g1")
-//	@KafkaHandler()
-//	public void listen(Integer payload) throws Exception { 
-//	
-//		logger.info("listen4 myTopic payload: " + payload);
-//
-//	}
-    
 
 	@KafkaHandler()
 	public void listen(DemoKafkaDto msg,
@@ -106,54 +66,5 @@ public class MyKafkaListener {
 			);
 
 	}
-
-
-	@KafkaHandler()
-	public void listen(@Payload List<Demo2KafkaDto> msgs,
-			@Header(KafkaHeaders.OFFSET) long offsets,
-			@Header(name=KafkaHeaders.RECEIVED_MESSAGE_KEY, required=false) Integer key,
-			@Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition,
-			@Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
-			@Header(KafkaHeaders.RECEIVED_TIMESTAMP) long ts,
-			@Header("PayLoadType") byte[] payLoadType,
-			@Header("From") byte[] from,
-			@Header("To") byte[] to
-			) throws Exception { 
-	
-		StringBuffer sb = new StringBuffer();
-		for(Demo2KafkaDto dto : msgs) {
-			sb.append(dto.toString());
-			sb.append("\n");
-		}
-		logger.info("listen myTopic Demo2KafkaDto msg: " + sb.toString()
-			+ "\n  " + KafkaHeaders.OFFSET + ": " + offsets
-			+ "\n  " + KafkaHeaders.RECEIVED_MESSAGE_KEY + ": " + key
-			+ "\n  " + KafkaHeaders.RECEIVED_PARTITION_ID + ": " + partition
-			+ "\n  " + KafkaHeaders.RECEIVED_TOPIC + ": " + topic
-			+ "\n  " + KafkaHeaders.RECEIVED_TIMESTAMP + ": " + ts
-			+ "\n  " + "PayLoadType" + ": " + Utils.utf8(payLoadType)
-			+ "\n  " + "From" + ": " + Utils.utf8(from)
-			+ "\n  " + "To" + ": " + Utils.utf8(to)
-			);
-
-	}
-	
-//	@KafkaListener(topics = "myTopic", groupId="g1"
-//			 ,containerFactory = "kafkaJsonListenerContainerFactory"
-//			)
-//	@KafkaHandler()
-	public void listen(ConsumerRecord<String, String> cr) throws Exception { 
-	
-		logger.info("listen myTopic receive: " + cr.toString());
-
-	}
-    
-//	@KafkaListener(topics = "myTopic", groupId="g2")
-//	public void listen2(ConsumerRecord<String, String> cr) throws Exception { 
-//	
-//		logger.info("listen2 myTopic receive: " + cr.toString());
-//
-//	}
-    
     
 }
